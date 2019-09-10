a5117d6ba17008193bb4d6857d4e3ca77ab52766
hp <moemoelwin99@gmail.com>
2019-09-10 22:50:17 +0630

    first

---
+++ src/main/java/com/example/firstpipeline/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.example.firstpipeline.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/")
+    public String Welcome(){
+        return "Hello AWS Pipeline";
+    }
+}
